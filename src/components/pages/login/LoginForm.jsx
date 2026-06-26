e1c5fb4a838026bae33f0419ba09c67bc21ccbb7
Kai Lam <kai_lam@hotmail.fr>
2026-06-26 13:07:31 +0200

    ✨ F02: LOGIN > add redirection to OrderPage on Submit with username

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
   // state
   const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
   // comportement
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Bonjour ${inputValue}`);
     setInputValue("");
+    navigate(`/order/${inputValue}`);
   };
 
   const handleChange = (event) => {
@@ -27,7 +28,6 @@ export default function LoginForm() {
         onChange={handleChange}
       />
       <button>Accédez à votre espace</button>
-      <Link to="/order">Vers OrderPage</Link>
     </form>
   );
 }
