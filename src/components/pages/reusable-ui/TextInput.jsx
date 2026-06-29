2ba0c25d9cc781aaf1ccd0dc5d026d57abe2cdb4
Kai Lam <kai_lam@hotmail.fr>
2026-06-29 22:54:23 +0200

    💄 F03: LOGIN > LoginForm > Replace hard coded values with Design System values

--- src/components/pages/reusable-ui/TextInput.jsx
+++ src/components/pages/reusable-ui/TextInput.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../../../theme";
 
 export default function TextInput({ value, onChange, Icon, ...extraProps }) {
   return (
@@ -11,27 +12,27 @@ export default function TextInput({ value, onChange, Icon, ...extraProps }) {
 
 const TextInputStyled = styled.div`
   background-color: #fff;
-  border-radius: 5px;
+  border-radius: ${theme.borderRadius.round};
   display: flex;
   align-items: center;
   padding: 18px 24px;
   margin: 18px 0;
 
   .icon {
-    font-size: 15px;
+    font-size: ${theme.fonts.size.P0};
     margin-right: 8px;
-    color: #93a2b1;
+    color: ${theme.colors.greySemiDark};
   }
 
   input {
     border: none;
-    font-size: 15px;
-    color: #17161a;
+    font-size: ${theme.fonts.size.P0};
+    color: ${theme.colors.dark};
     width: 100%;
-  }
 
-  &::placeholder {
-    background: white;
-    color: lightgray;
+    &::placeholder {
+      background: ${theme.colors.white};
+      color: ${theme.colors.greyMedium};
+    }
   }
 `;
