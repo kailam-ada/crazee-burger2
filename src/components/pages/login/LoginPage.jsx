0c543750833dc6924d05d52c366e6918e992eba3
Kai Lam <kai_lam@hotmail.fr>
2026-06-29 13:48:07 +0200

    💄 F03: LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -13,10 +13,14 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  background-image:
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("/images/burger-and-fries-background.jpg");
+  background-size: cover;
 `;
