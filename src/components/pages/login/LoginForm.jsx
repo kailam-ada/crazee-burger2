0ea5c975cb8a37a857f0dccc605beaefb35cf291
Kai Lam <kai_lam@hotmail.fr>
2026-06-29 14:26:40 +0200

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -20,7 +20,7 @@ export default function LoginForm() {
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <input
         type="text"
@@ -35,4 +35,27 @@ export default function LoginForm() {
 
 const LoginFormStyled = styled.form`
   background: green;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2 rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
 `;
