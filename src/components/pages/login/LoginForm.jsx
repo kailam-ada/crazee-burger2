38df389700badc59c48ddcbc10cc1031e5b1db50
Kai Lam <kai_lam@hotmail.fr>
2026-06-29 14:46:17 +0200

    💄 F03: LOGIN > LoginForm > input > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BsPersonCircle } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
@@ -22,12 +23,15 @@ export default function LoginForm() {
       <h1>Bienvenue chez nous !</h1>
       <hr />
       <h2>Connectez-vous</h2>
-      <input
-        type="text"
-        required
-        placeholder="Entrez votre prénom"
-        onChange={handleChange}
-      />
+      <div className="input-with-icon">
+        <BsPersonCircle className="icon" />
+        <input
+          type="text"
+          required
+          placeholder="Entrez votre prénom"
+          onChange={handleChange}
+        />
+      </div>
       <button>Accéder à mon espace</button>
     </LoginFormStyled>
   );
@@ -39,7 +43,7 @@ const LoginFormStyled = styled.form`
   max-width: 500px;
   min-width: 400px;
   margin: 0px auto;
-  padding: 2.5rem 2 rem;
+  padding: 2.5rem 2rem;
   border-radius: 5px;
   font-family: "Amatic SC", cursive;
 
@@ -57,5 +61,31 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
+  }
+
+  .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+  }
+
+  .icon {
+    font-size: 15px;
+    margin-right: 8px;
+    color: #93a2b1;
+  }
+
+  input {
+    border: none;
+    font-size: 15px;
+    color: #17161a;
+  }
+
+  &::placeholder {
+    background: white;
+    color: lightgray;
   }
 `;
