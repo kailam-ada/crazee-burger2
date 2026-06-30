c9eefee99bc3accb2155d0558e72e56314f0942b
Kai Lam <kai_lam@hotmail.fr>
2026-06-30 10:53:44 +0200

    ♻️ F04: ORDER > create Navbar and Main

---
+++ src/components/pages/order/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Navbar({ username }) {
+  return (
+    <NavbarStyled className="navbar">
+      <h1>Bonjour {username}</h1>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </NavbarStyled>
+  );
+}
+
+const NavbarStyled = styled.nav`
+  background: blue;
+  height: 10vh;
+`;
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,19 +1,16 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 export default function OrderPage() {
   const { username } = useParams();
   return (
     <OrderPageStyled>
       <div className="container">
-        <div className="navbar">
-          <h1>Bonjour {username}</h1>
-          <Link to="/">
-            <button>Déconnexion</button>
-          </Link>
-        </div>
-        <div className="main">Main</div>
+        <Navbar username={username} />
+        <Main />
       </div>
     </OrderPageStyled>
   );
@@ -32,14 +29,5 @@ const OrderPageStyled = styled.div`
     width: 1400px;
     display: flex;
     flex-direction: column;
-
-    .navbar {
-      background: blue;
-      height: 10vh;
-    }
-    .main {
-      background: green;
-      flex: 1;
-    }
   }
 `;
