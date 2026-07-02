c6670bfd155ca36b0d033a3b0d32f26eb962a68c
Kai Lam <kai_lam@hotmail.fr>
2026-07-02 23:30:40 +0200

    🚧 F05: ORDER > MAIN > Add grid layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -2,7 +2,12 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled className="main"></MainStyled>;
+  return (
+    <MainStyled className="main">
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
@@ -11,4 +16,15 @@ const MainStyled = styled.div`
   flex: 1;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  display: grid;
+  grid-template-columns: 25% 1fr;
+
+  .basket {
+    background: pink;
+  }
+
+  .menu {
+    background: purple;
+  }
 `;
