2528bfe941d2b2ea55846655f2b9dc2e72bbc5f3
Kai Lam <kai_lam@hotmail.fr>
2026-07-03 15:01:25 +0200

    ♻️ F05: ORDER > MENU > Create <Product/>

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
+import Product from "./Product";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -9,18 +10,11 @@ export default function Menu() {
     <MenuStyled className="menu">
       {menu.map((produit) => {
         return (
-          <div className="produit">
-            <div className="image">
-              <img src={produit.imageSource} alt={produit.title} />
-            </div>
-            <div className="info-text">
-              <div className="title">{produit.title}</div>
-              <div className="description">
-                <div className="price">{produit.price}</div>
-                <button className="add-button">Ajouter</button>
-              </div>
-            </div>
-          </div>
+          <Product
+            imageSource={produit.imageSource}
+            price={produit.price}
+            title={produit.title}
+          />
         );
       })}
     </MenuStyled>
@@ -45,10 +39,15 @@ const MenuStyled = styled.div`
       border: 1px solid fuchsia;
       width: 200px;
       height: 145px;
+
       img {
         width: 100%;
         height: 100%;
       }
+    }
+
+    .description {
+      border: 1px solid fuchsia;
     }
   }
 `;
