dd871050cdd65a7608af18f6ede0a4a59d615831
Kai Lam <kai_lam@hotmail.fr>
2026-07-03 13:07:26 +0200

    💄 F05 > ORDER > MENU > Add final style

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { theme } from "../../../../theme";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -14,7 +15,7 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  background: purple;
+  background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   padding: 50px 50px 150px;
   display: grid;
--- src/components/pages/order/Navbar/Navbar.jsx
+++ src/components/pages/order/Navbar/Navbar.jsx
@@ -21,6 +21,7 @@ const NavbarStyled = styled.nav`
   padding: 0 20px;
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
+  border-bottom: ${theme.colors.greyLight};
 
   .logo-order-page {
     cursor: pointer;
