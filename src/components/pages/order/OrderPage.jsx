67e7ca3666c3e4602c81e9eedf3c97a76f6638bd
Kai Lam <kai_lam@hotmail.fr>
2026-07-08 17:14:59 +0200

    🐛 F05: ORDER > OrderPage > Fix case-sensitivity issue in the import path

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { theme } from "../../../theme";
 import Main from "./Main/Main";
-import Navbar from "./navbar/Navbar";
+import Navbar from "./Navbar/Navbar";
 
 export default function OrderPage() {
   const { username } = useParams();
