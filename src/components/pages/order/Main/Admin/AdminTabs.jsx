60f0a820ef1b54cd5ff8a5bbb2981eff5bb9927e
Kai Lam <kai_lam@hotmail.fr>
2026-07-15 15:45:56 +0200

    ♻️ F07: ADMIN > Replace selectAdd and selectEdit with selectTab

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -29,6 +29,20 @@ export default function AdminTabs() {
     setIsEditSelected(false);
   };
 
+  const selectTab = (tabSelected) => {
+    setIsCollapsed(false);
+
+    if (tabSelected === "edit") {
+      setIsEditSelected(true);
+      setIsAddSelected(false);
+    }
+
+    if (tabSelected === "add") {
+      setIsAddSelected(true);
+      setIsEditSelected(false);
+    }
+  };
+
   return (
     <AdminTabsStyled>
       <Tab
@@ -40,13 +54,17 @@ export default function AdminTabs() {
       <Tab
         label="Ajouter un produit"
         Icon={<AiOutlinePlus />}
-        onClick={selectAddTab}
+        onClick={() => {
+          selectTab("add");
+        }}
         className={isAddSelected ? "is-active" : ""}
       />
       <Tab
         label="Modifier un produit"
         Icon={<MdModeEditOutline />}
-        onClick={selectEditTab}
+        onClick={() => {
+          selectTab("edit");
+        }}
         className={isEditSelected ? "is-active" : ""}
       />
     </AdminTabsStyled>
