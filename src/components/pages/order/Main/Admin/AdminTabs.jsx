c8f22f622b7eefb66be4fe0bbfccd8a3e57f6d92
Kai Lam <kai_lam@hotmail.fr>
2026-07-15 10:52:31 +0200

    ✨ F07: ADMIN > Tab > Add label

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import Tab from "../../../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { theme } from "../../../../../theme";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 
 export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
   // const handleClick = () => {
@@ -11,7 +13,20 @@ export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
   return (
     <AdminTabsStyled>
       <Tab
+        label=""
         Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      <Tab
+        label="Ajouter un produit"
+        Icon={<AiOutlinePlus />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      <Tab
+        label="Modifier un produit"
+        Icon={<MdModeEditOutline />}
         onClick={() => setIsCollapsed(!isCollapsed)}
         className={isCollapsed ? "is-active" : ""}
       />
@@ -27,5 +42,9 @@ const AdminTabsStyled = styled.div`
     background: ${theme.colors.background_dark};
     border-color: ${theme.colors.background_dark};
     color: ${theme.colors.white};
+  }
+
+  button {
+    margin-left: 1px;
   }
 `;
--- src/components/pages/reusable-ui/Tab.jsx
+++ src/components/pages/reusable-ui/Tab.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
 
-export default function Tab({ Icon, onClick, className }) {
+export default function Tab({ Icon, onClick, className, label }) {
   return (
     <TabStyled onClick={onClick} className={className}>
       <div className="icon">{Icon}</div>
+      {label && <span className="label">{label}</span>}
     </TabStyled>
   );
 }
@@ -43,9 +44,14 @@ const TabStyled = styled.button`
 
   &:hover {
     border-bottom: 2px solid ${theme.colors.white};
+    text-decoration: underline;
   }
 
   .icon {
     display: flex;
+  }
+
+  .label {
+    margin-left: 13px;
   }
 `;
