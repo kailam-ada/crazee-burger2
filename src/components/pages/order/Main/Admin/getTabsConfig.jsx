08e0199f8352c469efbc812936a1ac23340f8b24
Kai Lam <kai_lam@hotmail.fr>
2026-07-16 09:54:38 +0200

    🚧 F07: ADMIN > Tabs > Add getTabsConfig file

---
+++ src/components/pages/order/Main/Admin/getTabsConfig.jsx
@@ -0,0 +1,25 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { MdModeEditOutline } from "react-icons/md";
+
+export const getTabsConfig = (currentTabSelected) => [
+  //   {
+  //     index: "chevronUpDown",
+  //     label: "",
+  //     Icon: isCollapsed ? <FiChevronUp /> : <FiChevronDown />,
+  //     onClick: () => setIsCollapsed(!isCollapsed),
+  //     className: isCollapsed ? "is-active" : "",
+  //   },
+  {
+    index: "add",
+    label: "Ajouter un produit",
+    Icon: <AiOutlinePlus />,
+    className: currentTabSelected === "add" ? "is-active" : "",
+  },
+  {
+    index: "edit",
+    label: "Modifier un produit",
+    Icon: <MdModeEditOutline />,
+    className: currentTabSelected === "edit" ? "is-active" : "",
+  },
+];
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -10,6 +10,7 @@ export default function OrderPage() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [isAddSelected, setIsAddSelected] = useState(true);
   const [isEditSelected, setIsEditSelected] = useState(false);
+  const [currentTabSelected, setCurrentTabSelected] = useState("add");
 
   const orderContextValue = {
     isAdminMode,
@@ -20,6 +21,8 @@ export default function OrderPage() {
     setIsAddSelected,
     isEditSelected,
     setIsEditSelected,
+    currentTabSelected,
+    setCurrentTabSelected,
   };
 
   return (
