9d7a40da394ef5781c4d54a90d0111773b75a76e
Kai Lam <kai_lam@hotmail.fr>
2026-07-17 11:48:28 +0200

    🔧 F08: Set up files to start ticket

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { fakeMenu } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
 import Card from "../../reusable-ui/Card";
 import { formatPrice } from "../../../../utils/maths";
 
 export default function Menu() {
-  const [menu, setMenu] = useState(fakeMenu2);
+  const [menu, setMenu] = useState(fakeMenu.MEDIUM);
   return (
     <MenuStyled className="menu">
       {menu.map(({ id, title, imageSource, price }) => {
