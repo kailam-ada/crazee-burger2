3e4f129a1abb20bc5a0f744e1db6169e55b73170
Kai Lam <kai_lam@hotmail.fr>
2026-08-20 10:38:13 +0200

    ✨ F08: ADMIN > AddForm > Add coming-soon

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -5,6 +5,8 @@ import Card from "../../reusable-ui/Card";
 import { formatPrice } from "../../../../utils/maths";
 import OrderContext from "../../../../context/OrderContext";
 
+const DEFAULT_IMAGE = "/images/coming-soon.png";
+
 export default function Menu() {
   const { menu } = useContext(OrderContext);
 
@@ -15,7 +17,7 @@ export default function Menu() {
           <Card
             key={id}
             title={title}
-            imageSource={imageSource}
+            imageSource={imageSource ? imageSource : DEFAULT_IMAGE}
             leftDescription={formatPrice(price)}
           />
         );
