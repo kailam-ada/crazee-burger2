5a77baaeb3ea950346f8295793eb8c7cfae1b930
Kai Lam <kai_lam@hotmail.fr>
2026-08-20 11:21:39 +0200

    ✨ F08: ADMIN > AddForm > Reset form after submission

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -23,6 +23,7 @@ export default function AddForm() {
     };
 
     handleAdd(newProductToAdd);
+    setNewProduct(EMPTY_PRODUCT);
   };
 
   const handleChange = (event) => {
@@ -44,21 +45,21 @@ export default function AddForm() {
           name="title"
           value={newProduct.title}
           type="text"
-          placeholder="Name"
+          placeholder="Nom du produit (ex: Super Burger)"
           onChange={handleChange}
         />
         <input
           name="imageSource"
           value={newProduct.imageSource}
           type="text"
-          placeholder="Image URL"
+          placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
           onChange={handleChange}
         />
         <input
           name="price"
           value={newProduct.price ? newProduct.price : ""}
           type="text"
-          placeholder="Price"
+          placeholder="Prix"
           onChange={handleChange}
         />
       </div>
