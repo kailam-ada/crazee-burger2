2ff8a2bd51e83d60a87b21c2aa022d60a0cd4545
Kai Lam <kai_lam@hotmail.fr>
2026-08-20 14:38:32 +0200

    ✨ F08: CARD > Add delete button (style only)

--- src/components/pages/reusable-ui/Card.jsx
+++ src/components/pages/reusable-ui/Card.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
 import PrimaryButton from "./PrimaryButton";
+import { TiDelete } from "react-icons/ti";
 
 export default function Card({ imageSource, leftDescription, title }) {
   return (
     <CardStyled className="produit">
+      <button className="delete-button" aria-label="delete-button">
+        <TiDelete className="icon" />
+      </button>
       <div className="image">
         <img src={imageSource} alt={title} />
       </div>
@@ -32,6 +36,34 @@ const CardStyled = styled.div`
   padding-bottom: 10px;
   box-shadow: ${theme.shadows.medium};
   border-radius: ${theme.borderRadius.extraRound};
+  position: relative;
+
+  .delete-button {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+    color: ${theme.colors.primary};
+    z-index: 2;
+    padding: 0;
+    border: none;
+    background: none;
+
+    .icon {
+      height: 100%;
+      width: 100%;
+    }
+
+    :hover {
+      color: ${theme.colors.red};
+    }
+
+    :active {
+      color: ${theme.colors.primary};
+    }
+  }
 
   .image {
     width: 100%;
