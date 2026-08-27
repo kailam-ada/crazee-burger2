7c1d8b5548f25b7f7f711fa726110b2917771696
Kai Lam <kai_lam@hotmail.fr>
2026-08-27 22:11:43 +0200

    🐛 F08: AdminPanel > Fix TextInput height by adding box-sizing

--- src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
@@ -20,6 +20,7 @@ const AdminPanelStyled = styled.div`
   background: ${theme.colors.white};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
-  height: 250px;
+  height: 240px;
+  box-sizing: border-box;
   padding: 30px 5%;
 `;
